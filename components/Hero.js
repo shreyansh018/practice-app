b1453748fac331dadf2d3a8121946081c539352b
Shreyansh <shreyansh.singhrajput2019@gmail.com>
2023-06-30 11:41:54 +0530

    update

--- components/Hero.js
+++ components/Hero.js
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
 export default function Hero() {
-  return <Image src="/../public/image/banner.jpg" width={1920} height={400} />;
+  return (
+    <Image
+      src="https://t4.ftcdn.net/jpg/02/18/18/55/360_F_218185587_P4zituDtWJOfClUKL6merI0BgLMIxoeC.jpg"
+      width={1920}
+      height={400}
+    />
+  );
 }
